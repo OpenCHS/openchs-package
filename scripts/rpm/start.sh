0c6dccd2122e8d41d77504159523eda48a3de1fa
Arjun <arjunk@thoughtworks.com>
2017-07-16 10:37:36 +0530

    appliction now runs fine with rules. Postgres dependency added for installation

--- scripts/rpm/start.sh
+++ scripts/rpm/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nohup java $SERVER_OPTS -jar /opt/openchs/lib/openchs.jar > /var/log/openchs/openchs.log 2>&1 &
+cd /opt/openchs && nohup java $SERVER_OPTS -jar /opt/openchs/lib/openchs.jar > /var/log/openchs/openchs.log 2>&1 &
 echo $! > /var/run/openchs/openchs.pid
