84cd3335c9e90c76f82e32ccb693148fa7fb7ec9
Arjun <arjunk@thoughtworks.com>
2017-08-05 19:56:27 +0530

    WIP : Making deployment easier

---
+++ build_and_fetch_artifacts.sh
@@ -0,0 +1,8 @@
+cd ../openchs-server && git stash && git pull && mvn clean install -DskipTests
+cd ../openchs-package && cp ../openchs-server/target/openchs-server*jar ./openchs/resources/
+cd ../openchs-client && git stash && git pull && make release KEYSTORE_PASSWORD=helloworld KEY_PASSWORD=helloworld
+cd ../openchs-package && cp ../openchs-client/android/app/build/outputs/apk/app-release.apk ./openchs/resources/app.apk
+cd ../lokbiradari-vhw && git stash && git pull && make package_health_modules && make package_impl
+cd ../openchs-package && cp ../lokbiradari-vhw/output/{openchs_health_modules.tar.gz, openchs_impl.tar.gz} ./openchs/resources/
+scp ./openchs/build/resources/openchs*rpm openchsvagrant:/tmp/
+
