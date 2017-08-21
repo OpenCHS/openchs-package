4df0fa4c0da9c6507d3e7fe13dfff4fb7cdb51e8
Arjun <arjunk@thoughtworks.com>
2017-08-21 09:01:56 +0530

    Accomodating the change in repo due to extraction of impl to new repo

--- build_and_fetch_resources.sh
+++ build_and_fetch_resources.sh
@@ -2,7 +2,8 @@ cd ../openchs-server && git stash && git pull && mvn clean install -DskipTests
 cd ../openchs-package && cp ../openchs-server/openchs-server-api/target/openchs-server*jar ./openchs/resources/
 cd ../openchs-client && git stash && git pull && make release KEYSTORE_PASSWORD=helloworld KEY_PASSWORD=helloworld
 cd ../openchs-package && cp ../openchs-client/android/app/build/outputs/apk/app-release.apk ./openchs/resources/app.apk
-cd ../lokbiradari-vhw && git stash && git pull && make package_health_modules && make package_impl
-cd ../openchs-package && cp ../lokbiradari-vhw/output/openchs_health_modules.tar.gz  ./openchs/resources/
-cp ../lokbiradari-vhw/output/openchs_impl.tar.gz ./openchs/resources/
+cd ../openchs-health-modules && git stash && git pull && make deployable
+cp output/openchs_health_modules.tar.gz ../openchs-package/openchs/resources/
+cd ../lbp-arogyadoot && git stash && git pull && make deployable
+cd ../openchs-package && cp ../lbp-arogyadoot/output/openchs_impl.tar.gz ./openchs/resources/
 
